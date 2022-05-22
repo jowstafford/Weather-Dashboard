3bfad08c3465af664a5e1429237f15d82c01622a
jowstafford <91708768+jowstafford@users.noreply.github.com>
2022-05-21 22:16:09 -0500

    added currentposition and weatherfetch function

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -42,3 +42,45 @@ var cityNameArray = [];
 if (window.localStorage.getItem("cityName")) {
   cityNameArray.push(...JSON.parse(window.localStorage.getItem("cityName")));
 }
+
+var weatherFetch = (lat, lon) => {
+  var latLon =
+    "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+    lat +
+    "&lon=" +
+    lon +
+    "&units=imperial&exclude=minutely,hourly,alerts&appid=1d74fce1be8d5312d3fcc06f18c3880f";
+  console.log(latLon);
+
+  fetch(latLon).then((response) => {
+    response.json().then((data) => {
+      console.log(data);
+      conditionVariables.id = data.current.weather[0].id;
+      conditionVariables.temp = data.current.temp;
+      conditionVariables.wind = data.current.wind_speed;
+      conditionVariables.humidity = data.current.humidity;
+      conditionVariables.UVIndex = data.current.uvi;
+      conditionVariables.clouds = data.current.clouds;
+      finalWeather();
+    });
+  });
+};
+
+var currentPosition = (cityInput) => {
+  cityInput = cityInputEl.value;
+  if (cityInput) {
+    var apiSearch =
+      "https://api.openweathermap.org/geo/1.0/direct?q=" +
+      cityInput +
+      "&limit=1&appid=1d74fce1be8d5312d3fcc06f18c3880f";
+    fetch(apiSearch).then((response) => {
+      response.json().then((data) => {
+        lat = data[0].lat;
+        lon = data[0].lon;
+        forecastInfo(lat, lon);
+      });
+    });
+  } else {
+    alert("Please Enter City Name");
+  }
+};
