645c09915ac15f053252b211d5ecda5de7dcb698
jowstafford <91708768+jowstafford@users.noreply.github.com>
2022-05-22 20:32:18 -0500

    finished weather dashbaord. fixed inputinfo design. added font awesomme icons

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -38,11 +38,6 @@ var cityCordinates = (cityInput) => {
   }
 };
 
-var cityNameArray = [];
-if (window.localStorage.getItem("cityName")) {
-  cityNameArray.push(...JSON.parse(window.localStorage.getItem("cityName")));
-}
-
 var weatherFetch = (lat, lon) => {
   var latLon =
     "https://api.openweathermap.org/data/2.5/onecall?lat=" +
@@ -66,25 +61,6 @@ var weatherFetch = (lat, lon) => {
   });
 };
 
-var currentPosition = (cityInput) => {
-  cityInput = cityInputEl.value;
-  if (cityInput) {
-    var apiSearch =
-      "https://api.openweathermap.org/geo/1.0/direct?q=" +
-      cityInput +
-      "&limit=1&appid=1d74fce1be8d5312d3fcc06f18c3880f";
-    fetch(apiSearch).then((response) => {
-      response.json().then((data) => {
-        lat = data[0].lat;
-        lon = data[0].lon;
-        forecastInfo(lat, lon);
-      });
-    });
-  } else {
-    alert("Please Enter City Name");
-  }
-};
-
 var finalWeather = () => {
   var CityElement = document.querySelector("#inputInfo");
   var CityUVElement = document.createElement("p");
@@ -92,7 +68,7 @@ var finalWeather = () => {
   var CityTempElement = document.createElement("p");
   var CityWindElement = document.createElement("p");
   var CityHumidityElement = document.createElement("p");
-  var CityCloudsElement = document.createElement("i");
+  var CityCloudsElement = document.createElement("p");
   CityElement.innerHTML = "";
   CityInputElement.textContent = cityInput;
   CityTempElement.textContent =
@@ -107,6 +83,7 @@ var finalWeather = () => {
   CityElement.appendChild(CityWindElement);
   CityElement.appendChild(CityHumidityElement);
   CityElement.appendChild(CityUVElement);
+  CityElement.appendChild(CityCloudsElement);
 
   if (conditionVariables.clouds == 100) {
     CityCloudsElement.classList.add("fa-solid");
@@ -129,6 +106,77 @@ var finalWeather = () => {
   }
 };
 
+var cityNameArray = [];
+if (window.localStorage.getItem("cityName")) {
+  cityNameArray.push(...JSON.parse(window.localStorage.getItem("cityName")));
+}
+
+var currentPosition = (cityInput) => {
+  cityInput = cityInputEl.value;
+  if (cityInput) {
+    var apiSearch =
+      "https://api.openweathermap.org/geo/1.0/direct?q=" +
+      cityInput +
+      "&limit=1&appid=1d74fce1be8d5312d3fcc06f18c3880f";
+    fetch(apiSearch).then((response) => {
+      response.json().then((data) => {
+        lat = data[0].lat;
+        lon = data[0].lon;
+        forecastInfo(lat, lon);
+      });
+    });
+  } else {
+    alert("Please Enter City Name");
+  }
+};
+
+var forecastInfo = (lat, lon) => {
+  var url =
+    "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+    lat +
+    "&lon=" +
+    lon +
+    "&exclude=current,minutely,hourly&appid=1d74fce1be8d5312d3fcc06f18c3880f";
+  weatherDisplay(url);
+};
+
+var WeekdayList = (dayNumber) => {
+  var dayElement = new Array(7);
+  dayElement[0] = "Sunday";
+  dayElement[1] = "Monday";
+  dayElement[2] = "Tuesday";
+  dayElement[3] = "Wednesday";
+  dayElement[4] = "Thursday";
+  dayElement[5] = "Friday";
+  dayElement[6] = "Saturday";
+
+  return dayElement[dayNumber];
+};
+
+var trueTemp = (kelvin) => {
+  const celsius = kelvin - 273;
+  const fahrenheit = Math.floor(celsius * (9 / 5) + 32);
+  return fahrenheit;
+};
+
+var trueTime = (timeData) => {
+  var currentTime = new Date(timeData * 1000);
+  var currentHours = currentTime.getHours();
+  var currentMinutes = "";
+  if (currentTime.getMinutes() < 10) {
+    currentMinutes = "0" + currentTime.getMinutes();
+  } else {
+    currentMinutes = currentTime.getMinutes();
+  }
+  return currentHours + ":" + currentMinutes;
+};
+
+async function weatherDisplay(url) {
+  var weatherData = await fetch(url);
+  var weatherElement = await weatherData.text();
+  parseWeather(weatherElement);
+}
+
 var parseWeather = (weatherElement) => {
   var weatherJSON = JSON.parse(weatherElement);
   var dailyForecast = weatherJSON.daily;
@@ -150,10 +198,35 @@ var parseWeather = (weatherElement) => {
       highTemperature,
       lowTemperature,
       windSpeed,
+      windGust,
       Weekdays,
       humidity,
-      windGust,
       UVIndex
     );
   }
+};
+
+var displayWeatherDay = (
+  highTemperature,
+  lowTemperature,
+  windSpeed,
+  windGust,
+  Weekdays,
+  humidity,
+  UVIndex
+) => {
+  var displayElement =
+    "<div class='forecast'/>";
+  displayElement += "<h2>" + Weekdays + "</h2>";
+  displayElement +=
+    "<p>Temperature: " + highTemperature + "°F to " + lowTemperature + "°F</p>";
+  displayElement += "<p>Humidity: " + humidity + "%</p>";
+  displayElement +=
+    "<p>Wind Speeds: " +
+    Math.round(windSpeed) +
+    " to " +
+    Math.round(windGust) +
+    " MPH </p> ";
+  displayElement += "<p>UV Index: " + UVIndex + "</p></div>";
+  document.getElementById("forecastInfo").innerHTML += displayElement;
 };
